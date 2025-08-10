cdf589ca130e44d8a86fd8e77e254dc814dd967e
JJeset <arkhip57@gmail.com>
2025-08-10 18:31:57 +0300

    bruh update

--- Home-page.php
+++ Home-page.php
@@ -9,12 +9,20 @@ get_header(); ?>
 <main id="primary" class="site-main">
 
   <div class="intro">
-    <div class="container">
-      <div class="intro-up"></div>
-      <h1 class="intro-title">Каждый месяц — <br>новое кофейное <br>открытие</h1>
-
+    <div class="container main-container">
+      <div class="intro-text">
+        <h1 class="intro-title">Каждый месяц — <br>новое кофейное <br>открытие</h1>
+        <h2 class="intro-subtitle">Первая в России независимая подписка на зерновой кофе</h2>
+        <button class="button button_intro-production">
+          <span class="button-text">к продукции</span>
+        </button>
+      </div>
+    </div>
+    <div class="intro-image">
+      <img src="<?php echo get_template_directory_uri(); ?>/images/quality-sert.svg">
     </div>
     <img class="intro-wave" src="<?php echo get_template_directory_uri(); ?>/images/wave-effect.svg">
+  </div>
   </div>
 
   <?php the_content(); ?>
